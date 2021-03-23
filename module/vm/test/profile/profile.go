240c58fb65b8146079e99e40a73516f27fdc2d72
tianle han <danahan@tencent.com>
2021-03-23 13:58:19 +0800

    add report for profile

--- module/vm/test/profile/profile.go
+++ module/vm/test/profile/profile.go
@@ -13,6 +13,7 @@ import (
     "log"
     "net/http"
     _ "net/http/pprof"
+    "os"
     "strings"
     "sync"
     "sync/atomic"
@@ -74,6 +75,7 @@ func startPerf() {
     }
 
     finishNum := int64(0)
+    var gas int64
     start := time.Now().UnixNano() / 1e6
     for i := int64(0); i < totalCallTimes; {
         var createNum int64
@@ -89,30 +91,57 @@ func startPerf() {
             wg.Add(1)
             go func() {
                 defer wg.Done()
+                var result *commonPb.ContractResult
                 if vmTypeInt == 0 {
-                    invokeContractOfGasm("increase", contractId, txContext, byteCode)
+                    result = invokeContractOfGasm("increase", contractId, txContext, byteCode)
                 } else {
-                    invokeContractOfWasmer("increase", contractId, txContext, pool, byteCode)
+                    result = invokeContractOfWasmer("increase", contractId, txContext, pool, byteCode)
                 }
 
-                end := time.Now().UnixNano() / 1e6
+                //end := time.Now().UnixNano() / 1e6
                 finished := atomic.AddInt64(&finishNum, 1)
-                //atomic.AddInt64(&finishNum, 1)
-                if (end-start)/1000 > 0 && finished % 100 == 0 {
-                   fmt.Printf("【tps】 %d/s 【spend】%dms, finished=%d \n",
-                       finished * 1000/int64(end-start), end-start, finished)
+                if finished == 1 {
+                    gas = result.GasUsed
                 }
+                //if (end-start)/1000 > 0 && finished % 1000 == 0 {
+                //   fmt.Printf("【tps】 %d/s 【spend】%dms, finished=%d, used gas=%d\n",
+                //       finished * 1000/int64(end-start), end-start, finished, result.GasUsed)
+                //}
             }()
         }
         wg.Wait()
         end := time.Now().UnixNano() / 1e6
         fmt.Printf("finished %d task in %dms, average tps is %d, totalCallTimes: %d, vmGoroutineNum: %d, " +
-            "createNum: %d, i: %d\n",
-            finishNum, end-start, finishNum * 1000/(end-start), totalCallTimes, vmGoroutineNum, createNum, i)
+           "createNum: %d, i: %d, used gas: %d\n",
+           finishNum, end-start, finishNum * 1000/(end-start), totalCallTimes, vmGoroutineNum, createNum, i, gas)
+    }
+    end := time.Now().UnixNano() / 1e6
+    f, err := os.OpenFile("Vm Performance Report.md", os.O_APPEND|os.O_CREATE, 0644)
+    if err != nil {
+        fmt.Println("open report file err", err)
+    }
+    defer f.Close()
+    if vmTypeInt == 0 {
+        fmt.Printf("## Gasm VM Benchmarks\n### Contract: %s\n### Tps: %d\n### Total call times: %d\n" +
+            "### Go routines(concurrent instances): %d\n### Spend time: %d\n### Used gas: %d\n",
+            wasmFilePath, finishNum * 1000/(end-start), finishNum, vmGoroutineNum, end-start, gas)
+        f.Write([]byte(fmt.Sprintf("## Gasm VM Benchmarks\n###Contract: %s\n" +
+            "### Tps: %d\n### Total call times: %d\n### Go routines(concurrent instances): %d\n" +
+            "### Spend time: %d\n### Used gas: %d\n",
+            wasmFilePath, finishNum*1000/(end-start), finishNum, vmGoroutineNum, end-start, gas)))
+    } else {
+        fmt.Printf("## Wasmer VM Benchmarks\n### Contract: %s\n### Tps: %d\n### Total call times: %d\n" +
+            "### Go routines: %d\n### Concurrent instances: 10\n### Spend time: %d\n### Used gas: %d\n",
+            wasmFilePath, finishNum * 1000/(end-start), finishNum, vmGoroutineNum, end-start, gas)
+        f.Write([]byte(fmt.Sprintf("## Wasmer VM Benchmarks\n### Contract: %s\n" +
+            "### Tps: %d\n### Total call times: %d\n### Go routines: %d\n### Concurrent instances: 10\n" +
+            "### Spend time: %d\n### Used gas: %d\n",
+            wasmFilePath, finishNum * 1000/(end-start), finishNum, vmGoroutineNum, end-start, gas)))
     }
 }
 
-func invokeContractOfGasm(method string, contractId *commonPb.ContractId, txContext protocol.TxSimContext, byteCode []byte) {
+func invokeContractOfGasm(method string, contractId *commonPb.ContractId, txContext protocol.TxSimContext,
+    byteCode []byte)  (contractResult *commonPb.ContractResult) {
    parameters := make(map[string]string)
    test.BaseParam(parameters)
    //parameters["contract_name"] = test.ContractNameTest
@@ -122,16 +151,17 @@ func invokeContractOfGasm(method string, contractId *commonPb.ContractId, txCont
    runtimeInstance := &gasm.RuntimeInstance{
        Log: logger.GetLogger(logger.MODULE_VM),
    }
-   runtimeInstance.Invoke(contractId, method, byteCode, parameters, txContext, 0)
+   return runtimeInstance.Invoke(contractId, method, byteCode, parameters, txContext, 0)
 }
 
-func invokeContractOfWasmer(method string, contractId *commonPb.ContractId, txContext protocol.TxSimContext, pool *wasmer.VmPoolManager, byteCode []byte) {
+func invokeContractOfWasmer(method string, contractId *commonPb.ContractId, txContext protocol.TxSimContext,
+    pool *wasmer.VmPoolManager, byteCode []byte) (contractResult *commonPb.ContractResult) {
     parameters := make(map[string]string)
     test.BaseParam(parameters)
     //parameters["key"] = "key"
 
     runtime, _ := pool.NewRuntimeInstance(contractId, txContext, byteCode)
-    runtime.Invoke(contractId, method, byteCode, parameters, txContext, 0)
+    return runtime.Invoke(contractId, method, byteCode, parameters, txContext, 0)
 }
 
 func Perf() *cobra.Command {
