34758269bf152532a079f533d64cff227d35621c
taifu <515789026@qq.com>
2021-04-02 21:05:28 +0800

    4节点DDL测试

--- main/script/start_local_tbft_4.sh
+++ main/script/start_local_tbft_4.sh
@@ -10,17 +10,17 @@ export PATH=$(dirname $PWD)/prebuilt/linux:$(dirname $PWD)/prebuilt/win64:$PATH
 export WASMER_BACKTRACE=1
 cd ..
 
-pid=`ps -ef | grep chainmaker | grep "\-c ../config/wx-org1/chainmaker.yml local-tbft" | grep -v grep |  awk  '{print $2}'`
+pid=`ps -ef | grep chainmaker | grep "\-c ../config-sql/wx-org1/chainmaker.yml local-tbft" | grep -v grep |  awk  '{print $2}'`
 if [ -z ${pid} ];then
-    nohup ./chainmaker start -c ../config/wx-org1/chainmaker.yml local-tbft > panic.log &
+    nohup ./chainmaker start -c ../config-sql/wx-org1/chainmaker.yml local-tbft > panic.log &
     echo "wx-org1 chainmaker is startting, pls check log..."
 else
     echo "wx-org1 chainmaker is already started"
 fi
 
-pid2=`ps -ef | grep chainmaker | grep "\-c ../config/wx-org2/chainmaker.yml local-tbft" | grep -v grep |  awk  '{print $2}'`
+pid2=`ps -ef | grep chainmaker | grep "\-c ../config-sql/wx-org2/chainmaker.yml local-tbft" | grep -v grep |  awk  '{print $2}'`
 if [ -z ${pid2} ];then
-    nohup ./chainmaker start -c ../config/wx-org2/chainmaker.yml local-tbft > panic.log &
+    nohup ./chainmaker start -c ../config-sql/wx-org2/chainmaker.yml local-tbft > panic.log &
     echo "wx-org2 chainmaker is startting, pls check log..."
 else
     echo "wx-org2 chainmaker is already started"
@@ -28,21 +28,21 @@ fi
 
 
 
-pid3=`ps -ef | grep chainmaker | grep "\-c ../config/wx-org3/chainmaker.yml local-tbft" | grep -v grep |  awk  '{print $2}'`
+pid3=`ps -ef | grep chainmaker | grep "\-c ../config-sql/wx-org3/chainmaker.yml local-tbft" | grep -v grep |  awk  '{print $2}'`
 if [ -z ${pid3} ];then
-    nohup ./chainmaker start -c ../config/wx-org3/chainmaker.yml local-tbft > panic.log &
+    nohup ./chainmaker start -c ../config-sql/wx-org3/chainmaker.yml local-tbft > panic.log &
     echo "wx-org3 chainmaker is startting, pls check log..."
 else
     echo "wx-org3 chainmaker is already started"
 fi
 
 
-pid4=`ps -ef | grep chainmaker | grep "\-c ../config/wx-org4/chainmaker.yml local-tbft" | grep -v grep |  awk  '{print $2}'`
+pid4=`ps -ef | grep chainmaker | grep "\-c ../config-sql/wx-org4/chainmaker.yml local-tbft" | grep -v grep |  awk  '{print $2}'`
 if [ -z ${pid4} ];then
-    nohup ./chainmaker start -c ../config/wx-org4/chainmaker.yml local-tbft > panic.log &
+    nohup ./chainmaker start -c ../config-sql/wx-org4/chainmaker.yml local-tbft > panic.log &
     echo "wx-org4 chainmaker is startting, pls check log..."
 else
     echo "wx-org4 chainmaker is already started"
 fi
 
-# nohup ./chainmaker start -c ../config/wx-org5/chainmaker.yml local-tbft > panic.log &
+# nohup ./chainmaker start -c ../config-sql/wx-org5/chainmaker.yml local-tbft > panic.log &
