1ef8c71580250815aeec98b46308e250ce2cabb5
yongxinyao <yyxyong@163.com>
2021-04-22 11:43:15 +0800

    Add logs;

--- module/consensus/chainedbft/impl.go
+++ module/consensus/chainedbft/impl.go
@@ -378,7 +378,7 @@ func (cbi *ConsensusChainedBftImpl) onFiredEvent(te *timeservice.TimerEvent) {
 		return
 	}
 
-	cbi.logger.Infof("receive time out event, state: %s, height: %d, level: %d", te.State.String(), te.Height, te.Level)
+	cbi.logger.Infof("receive time out event, state: %s, height: %d, level: %d, duration: %s", te.State.String(), te.Height, te.Level, te.Duration.String())
 	switch te.State {
 	case chainedbftpb.ConsStateType_NewLevel:
 		cbi.processNewLevel(te.Height, te.Level)
