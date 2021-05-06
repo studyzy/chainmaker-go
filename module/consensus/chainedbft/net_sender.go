d66fc6928718e74646ec38b065f06c1af81f7cbe
yongxinyao <yyxyong@163.com>
2021-05-06 16:51:39 +0800

    Add logs;

--- module/consensus/chainedbft/net_sender.go
+++ module/consensus/chainedbft/net_sender.go
@@ -46,6 +46,7 @@ func (cbi *ConsensusChainedBftImpl) signAndBroadcast(payload *chainedbftpb.Conse
 		return
 	}
 	peers := cbi.smr.peers()
+	cbi.logger.Debugf("peers count: %d in signAndBroadcast", len(peers))
 	for _, peer := range peers {
 		if peer.index == cbi.selfIndexInEpoch {
 			continue
@@ -55,6 +56,7 @@ func (cbi *ConsensusChainedBftImpl) signAndBroadcast(payload *chainedbftpb.Conse
 			Type:    net.NetMsg_CONSENSUS_MSG,
 			To:      peer.id,
 		}
+		cbi.logger.Debugf("broadcast proposal msg ...")
 		go cbi.msgbus.Publish(msgbus.SendConsensusMsg, msg)
 	}
 }
