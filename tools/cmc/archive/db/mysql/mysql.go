276f0087e1facdb6a5de10ef1c61a9ad9d1318c4
wanghongda <p_hongdwang@tencent.com>
2021-05-13 21:13:57 +0800

    impl cmc archive dump command

---
+++ tools/cmc/archive/db/mysql/mysql.go
@@ -0,0 +1,49 @@
+package mysql
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+const (
+	dbMaxIdleConns    = 10
+	dbMaxOpenConns    = 100
+	dbConnMaxLifetime = 0
+)
+
+// InitDb Connect db server and create database if not exists then switch to this database,
+// returns *gorm.DB, error
+func InitDb(user, password, host, port, dbName string, migrateLock bool) (*gorm.DB, error) {
+	dsn := user + ":" + password + "@tcp(" + host + ":" + port + ")/?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		//Logger: logger.Default.LogMode(logger.Silent),
+	})
+	if err != nil {
+		return nil, err
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+	_, err = sqlDB.Exec("CREATE DATABASE IF NOT EXISTS " + dbName)
+	if err != nil {
+		return nil, err
+	}
+	_, err = sqlDB.Exec("USE " + dbName)
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxIdleConns(dbMaxIdleConns)
+	sqlDB.SetMaxOpenConns(dbMaxOpenConns)
+	sqlDB.SetConnMaxLifetime(dbConnMaxLifetime)
+
+	// migrate lock table
+	if migrateLock {
+		err = db.AutoMigrate(&lock{})
+		if err != nil {
+			return nil, err
+		}
+	}
+	return db, nil
+}
--- tools/cmc/go.mod
+++ tools/cmc/go.mod
@@ -11,11 +11,15 @@ require (
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.7.0
-	google.golang.org/grpc v1.36.0
+	gorm.io/driver/mysql v1.0.6
+	gorm.io/gorm v1.21.9
 	vuvuzela.io/crypto v0.0.0-20190327123840-80a93a3ed1d6
 )
 
 replace (
+	#chainmaker.org/chainmaker-go/common => /Users/wanghongda/GoProjects/chainmaker-sdk-go/common
+	#chainmaker.org/chainmaker-sdk-go => /Users/wanghongda/GoProjects/chainmaker-sdk-go
+
 	chainmaker.org/chainmaker-go/common => ../../common
 	chainmaker.org/chainmaker-sdk-go => ../sdk
 )
