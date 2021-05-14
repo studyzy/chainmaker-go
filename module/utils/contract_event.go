dd2cd8a85280e92bf3b5f0e8b782b0c8b0dfbae2
boom <dongbo@hyperchain.cn>
2021-05-14 13:55:03 +0800

    add index in contract event db table

--- module/utils/contract_event.go
+++ module/utils/contract_event.go
@@ -14,8 +14,9 @@ import (
 	"strconv"
 )
 
-const TopicTableColumnDdl = `id bigint unsigned NOT NULL AUTO_INCREMENT,chain_id varchar(128),block_height bigint,tx_id varchar(64),event_index int,topic varchar(255),contract_name varchar(1000),contract_version varchar(128),data1 text(65535),data2 text(65535),data3 text(65535),data4 text(65535),data5 text(65535),data6 text(65535),data7 text(65535),data8 text(65535),data9 text(65535),data10 text(65535),data11 text(65535),data12 text(65535),data13 text(65535),data14 text(65535),data15 text(65535),data16 text(65535),`
-const TopicTableUniqueKey = `chain_id,block_height,tx_id,event_index`
+const TopicTableColumnDdl = `id bigint unsigned NOT NULL AUTO_INCREMENT,chain_id varchar(128),block_height bigint,tx_id varchar(64),event_index int,topic varchar(255),contract_name varchar(255),contract_version varchar(128),data1 text(65535),data2 text(65535),data3 text(65535),data4 text(65535),data5 text(65535),data6 text(65535),data7 text(65535),data8 text(65535),data9 text(65535),data10 text(65535),data11 text(65535),data12 text(65535),data13 text(65535),data14 text(65535),data15 text(65535),data16 text(65535),`
+const TopicTableUniqueKey = `UNIQUE KEY unique_index(chain_id,block_height,tx_id,event_index)`
+const TopicTableIndex = `INDEX index_chain_id (chain_id ASC),INDEX index_block_height (block_height ASC),INDEX index_tx_id (tx_id ASC),INDEX index_event_index (event_index ASC),INDEX index_topic (topic ASC),INDEX index_contract_name (contract_name ASC),INDEX index_contract_version (contract_version ASC)`
 
 func GenerateSaveContractEventDdl(t *commonPb.ContractEvent, chainId string, blockHeight int64, event_index int) string {
 	var saveDdl string
@@ -70,6 +71,6 @@ func GenerateCreateTopicTableDdl(t *commonPb.ContractEvent, chainId string) stri
 	tableName := fmt.Sprintf("%s_%s_%s", chainId, t.ContractName, t.Topic)
 	topicTableNameHash := sha256.Sum256([]byte(tableName))
 	topicTableNameHex := fmt.Sprintf("event%s", hex.EncodeToString(topicTableNameHash[:20])[5:])
-	createTopicTableSql = fmt.Sprintf("CREATE TABLE IF NOT EXISTS  %s (%s PRIMARY KEY (id),UNIQUE KEY(%s) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;", topicTableNameHex, TopicTableColumnDdl, TopicTableUniqueKey)
+	createTopicTableSql = fmt.Sprintf("CREATE TABLE IF NOT EXISTS  %s (%s PRIMARY KEY (id),%s,%s ) ENGINE=InnoDB DEFAULT CHARSET=utf8;", topicTableNameHex, TopicTableColumnDdl, TopicTableUniqueKey, TopicTableIndex)
 	return createTopicTableSql
 }
