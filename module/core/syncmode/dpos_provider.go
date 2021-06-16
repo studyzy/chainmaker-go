9b1ed3686dc8a0db518fc53d4bf09dda208ac8a9
yongxinyao <yyxyong@163.com>
2021-06-16 11:42:52 +0800

    Fix: fix compile;

--- module/core/syncmode/dpos_provider.go
+++ module/core/syncmode/dpos_provider.go
@@ -1,9 +1,9 @@
 package syncmode
 
 import (
+	"chainmaker.org/chainmaker-go/core/provider"
+	"chainmaker.org/chainmaker-go/core/provider/conf"
 	"chainmaker.org/chainmaker-go/protocol"
-	"chainmaker.org/chainmaker-go/provider"
-	"chainmaker.org/chainmaker-go/provider/conf"
 )
 
 const ConsensusTypeDPOS = "DPOS"
@@ -13,6 +13,6 @@ var NilDPOSProvider provider.CoreProvider = (*dposProvider)(nil)
 type dposProvider struct {
 }
 
-func (tp *dposProvider) NewCoreEngine (config *conf.CoreEngineConfig) (protocol.CoreEngine, error) {
+func (tp *dposProvider) NewCoreEngine(config *conf.CoreEngineConfig) (protocol.CoreEngine, error) {
 	return NewCoreEngine(config)
 }
