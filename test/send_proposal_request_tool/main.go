7b62cf6151ebdebe64f20201122efe2596ed8c1f
zitaozhang <zitaozhang@tencent.com>
2021-06-21 15:17:34 +0800

    renew tools

--- test/send_proposal_request_tool/main.go
+++ test/send_proposal_request_tool/main.go
@@ -102,6 +102,7 @@ type Result struct {
 	GovernanceInfo        *consensusPb.GovernanceContract `json:"governanceInfo,omitempty"`
 	HibeExecMsg           string                          `json:"hibe_exec_msg,omitempty"`
 	CertAddress           *evm.Address                    `json:"certAddress,omitempty"`
+	Result               string                          `json:"result,omitempty"`
 }
 
 type SimpleRPCResult struct {
@@ -245,6 +246,7 @@ func main() {
 	mainCmd.AddCommand(ERC20BalanceOf())
 	mainCmd.AddCommand(ERC20Owner())
 	mainCmd.AddCommand(ERC20Decimals())
+	mainCmd.AddCommand(ERC20Cert2Address())
 
 	//DPoS.Stake
 	mainCmd.AddCommand(StakeGetAllCandidates())
