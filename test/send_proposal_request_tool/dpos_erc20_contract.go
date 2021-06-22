407b0345760f34518fbe9150c4f3e1fc59ab8fea
yongxinyao <yyxyong@163.com>
2021-06-22 10:13:29 +0800

    feat: add interface GET_TOTAL_SUPPLY in erc20 system contract; update pb;
    test:
    
    add cmd in test tools;

--- test/send_proposal_request_tool/dpos_erc20_contract.go
+++ test/send_proposal_request_tool/dpos_erc20_contract.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+
 	"github.com/gogo/protobuf/proto"
 
 	commonPb "chainmaker.org/chainmaker-go/pb/protogo/common"
@@ -201,6 +202,32 @@ func ERC20Decimals() *cobra.Command {
 func decimals() error {
 	pairs := make([]*commonPb.KeyValuePair, 0)
 	payloadBytes, err := constructPayload(commonPb.ContractName_SYSTEM_CONTRACT_DPOS_ERC20.String(), commonPb.DPoSERC20ContractFunction_GET_DECIMALS.String(), pairs)
+	if err != nil {
+		log.Fatalf("create payload failed, err: %s", err)
+	}
+	resp, err := proposalRequest(sk3, client, commonPb.TxType_QUERY_SYSTEM_CONTRACT,
+		chainId, "", payloadBytes)
+	if err != nil {
+		return err
+	}
+	return processResult(resp, nil)
+}
+
+func ERC20Total() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "erc20Total",
+		Short: "total supply of erc20",
+		Long:  " get total supply of tokens",
+		RunE: func(_ *cobra.Command, _ []string) error {
+			return total()
+		},
+	}
+	return cmd
+}
+
+func total() error {
+	pairs := make([]*commonPb.KeyValuePair, 0)
+	payloadBytes, err := constructPayload(commonPb.ContractName_SYSTEM_CONTRACT_DPOS_ERC20.String(), commonPb.DPoSERC20ContractFunction_GET_TOTAL_SUPPLY.String(), pairs)
 	if err != nil {
 		log.Fatalf("create payload failed, err: %s", err)
 	}
