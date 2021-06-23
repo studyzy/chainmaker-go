189aa19ab9047d208e6661f8407946d06945971c
yongxinyao <yyxyong@163.com>
2021-06-23 15:30:06 +0800

    doc: update copyright;

--- module/vm/native/dpos_erc20_contract_test.go
+++ module/vm/native/dpos_erc20_contract_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright (C) BABEC. All rights reserved.
+Copyright (C) THL A29 Limited, a Tencent company. All rights reserved.
 
 SPDX-License-Identifier: Apache-2.0
 */
@@ -277,4 +277,3 @@ func (c *CacheMock) Keys() []string {
 	}
 	return sc
 }
-
