5e952b8b4ad3c98deb27edb1f0550201a4c06cb8
wanghongda <harryhdwang@tencent.com>
2021-07-21 14:45:15 +0800

    update cmc.query to v2

--- tools/cmc/query/query_block.go
+++ tools/cmc/query/query_block.go
@@ -25,7 +25,7 @@ func newQueryBlockByHeightOnChainCMD() *cobra.Command {
 		Long:  "query on-chain block by height",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			height, err := strconv.ParseInt(args[0], 10, 64)
+			height, err := strconv.ParseUint(args[0], 10, 64)
 			if err != nil {
 				return err
 			}
