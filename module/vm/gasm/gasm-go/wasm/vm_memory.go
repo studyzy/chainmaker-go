c938459ed283630df22f65eb6e69ecaa9d1b3ebe
via <overcc@icloud.com>
2021-07-30 11:50:41 +0800

    vm_memory update

--- module/vm/gasm/gasm-go/wasm/vm_memory.go
+++ module/vm/gasm/gasm-go/wasm/vm_memory.go
@@ -4,6 +4,8 @@ import (
 	"encoding/binary"
 )
 
+var defaultMax = uint32(65535)  // 4G
+
 func memoryBase(vm *VirtualMachine) uint64 {
 	vm.ActiveContext.PC++
 	_ = vm.FetchUint32() // ignore align
@@ -13,13 +15,13 @@ func memoryBase(vm *VirtualMachine) uint64 {
 
 func i32Load(vm *VirtualMachine) {
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, false)
 	vm.OperandStack.Push(uint64(binary.LittleEndian.Uint32(vm.Memory[base:])))
 }
 
 func i64Load(vm *VirtualMachine) {
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, false)
 	vm.OperandStack.Push(binary.LittleEndian.Uint64(vm.Memory[base:]))
 }
 
@@ -33,7 +35,7 @@ func f64Load(vm *VirtualMachine) {
 
 func i32Load8s(vm *VirtualMachine) {
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, false)
 	vm.OperandStack.Push(uint64(vm.Memory[base]))
 }
 
@@ -43,7 +45,7 @@ func i32Load8u(vm *VirtualMachine) {
 
 func i32Load16s(vm *VirtualMachine) {
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, false)
 	vm.OperandStack.Push(uint64(binary.LittleEndian.Uint16(vm.Memory[base:])))
 }
 
@@ -53,7 +55,7 @@ func i32Load16u(vm *VirtualMachine) {
 
 func i64Load8s(vm *VirtualMachine) {
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, false)
 	vm.OperandStack.Push(uint64(vm.Memory[base]))
 }
 
@@ -63,7 +65,7 @@ func i64Load8u(vm *VirtualMachine) {
 
 func i64Load16s(vm *VirtualMachine) {
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, false)
 	vm.OperandStack.Push(uint64(binary.LittleEndian.Uint16(vm.Memory[base:])))
 }
 
@@ -73,7 +75,7 @@ func i64Load16u(vm *VirtualMachine) {
 
 func i64Load32s(vm *VirtualMachine) {
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, false)
 	vm.OperandStack.Push(uint64(binary.LittleEndian.Uint32(vm.Memory[base:])))
 }
 
@@ -84,63 +86,63 @@ func i64Load32u(vm *VirtualMachine) {
 func i32Store(vm *VirtualMachine) {
 	val := vm.OperandStack.Pop()
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, true)
 	binary.LittleEndian.PutUint32(vm.Memory[base:], uint32(val))
 }
 
 func i64Store(vm *VirtualMachine) {
 	val := vm.OperandStack.Pop()
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, true)
 	binary.LittleEndian.PutUint64(vm.Memory[base:], val)
 }
 
 func f32Store(vm *VirtualMachine) {
 	val := vm.OperandStack.Pop()
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, true)
 	binary.LittleEndian.PutUint32(vm.Memory[base:], uint32(val))
 }
 
 func f64Store(vm *VirtualMachine) {
 	v := vm.OperandStack.Pop()
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, true)
 	binary.LittleEndian.PutUint64(vm.Memory[base:], v)
 }
 
 func i32Store8(vm *VirtualMachine) {
 	v := byte(vm.OperandStack.Pop())
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, true)
 	vm.Memory[base] = v
 }
 
 func i32Store16(vm *VirtualMachine) {
 	v := uint16(vm.OperandStack.Pop())
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, true)
 	binary.LittleEndian.PutUint16(vm.Memory[base:], v)
 }
 
 func i64Store8(vm *VirtualMachine) {
 	v := byte(vm.OperandStack.Pop())
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, true)
 	vm.Memory[base] = v
 }
 
 func i64Store16(vm *VirtualMachine) {
 	v := uint16(vm.OperandStack.Pop())
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, true)
 	binary.LittleEndian.PutUint16(vm.Memory[base:], v)
 }
 
 func i64Store32(vm *VirtualMachine) {
 	v := uint32(vm.OperandStack.Pop())
 	base := memoryBase(vm)
-	memorySafeCheck(vm, base)
+	safeStore(vm, base, true)
 	binary.LittleEndian.PutUint32(vm.Memory[base:], v)
 }
 
@@ -165,13 +167,26 @@ func memoryGrow(vm *VirtualMachine) {
 	vm.Gas += tableGas[OptCodeMemoryGrow] * uint64(n)
 }
 
-func memorySafeCheck(vm *VirtualMachine, base uint64) {
+func safeStore(vm *VirtualMachine, base uint64, isStore bool) {
 	if base >= uint64(len(vm.Memory)) {
+		if !isStore {
+			panic("Illegal memory operation ")
+			return
+		}
+
+		if vm.InnerModule.SecMemory[0].Max == nil {
+			vm.InnerModule.SecMemory[0].Max = &defaultMax
+		}
+
 		diff := base - uint64(len(vm.Memory)) + 1
-		vm.Memory = append(vm.Memory, make([]byte, diff)...)
-		if vm.InnerModule.SecMemory[0].Max != nil &&
-			uint64(uint32(len(vm.Memory)/vmPageSize)) > uint64(*(vm.InnerModule.SecMemory[0].Max)) {
+		preMemory := len(vm.Memory) + int(diff)
+		if uint64(uint32(preMemory/vmPageSize)) > uint64(*(vm.InnerModule.SecMemory[0].Max)) {
 			panic("out of vm max memory limit ")
+			return
+		}
+
+		if isStore {
+			vm.Memory = append(vm.Memory, make([]byte, diff)...)
 		}
 	}
 }
