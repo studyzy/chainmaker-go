6bbf74dea35fb4e7518b416da7fc9e2fee687c49
DevinZeng <studyzy@qq.com>
2021-08-03 14:12:52 +0800

    enable more lint check for modules.

--- module/vm/wxvm/xvm/compile/compile.go
+++ module/vm/wxvm/xvm/compile/compile.go
@@ -2,7 +2,7 @@
  * Copyright (C) BABEC. All rights reserved.
  * Copyright (C) THL A29 Limited, a Tencent company. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
-*/
+ */
 
 package compile
 
