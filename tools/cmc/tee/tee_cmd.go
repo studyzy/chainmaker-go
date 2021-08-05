405bbd306fa40e40cf53308214a3d0e27b200888
hongdacai <1526875642@qq.com>
2021-08-05 14:47:05 +0800

    bugfix in cmc/tee/*.go because sdk_interface change

--- tools/cmc/tee/tee_cmd.go
+++ tools/cmc/tee/tee_cmd.go
@@ -9,8 +9,11 @@ package tee
 
 import (
 	sdk "chainmaker.org/chainmaker/sdk-go"
+	"errors"
+	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+	"strings"
 )
 
 var (
@@ -25,6 +28,8 @@ var (
 	clientCrtFilePaths string
 	orgId              string
 	chainId            string
+	adminKeyFilePaths  string
+	adminCrtFilePaths  string
 )
 
 var (
@@ -44,6 +49,8 @@ func NewTeeCMD() *cobra.Command {
 	teeFlags.StringVar(&clientCrtFilePaths, "client-crt-file-paths", "", "specify client cert file paths, use ',' to separate")
 	teeFlags.StringVar(&orgId, "org-id", "", "specify the orgId, such as wx-org1.chainmaker.com")
 	teeFlags.StringVar(&chainId, "chain-id", "", "specify the chain id, such as: chain1, chain2 etc.")
+	teeFlags.StringVar(&adminKeyFilePaths, "admin-key-file-paths", "","specify admin key file paths, use ',' to separate")
+	teeFlags.StringVar(&adminCrtFilePaths, "admin-crt-file-paths", "", "specify admin cert file paths, use ',' to separate")
 
 	teeCmd.AddCommand(uploadCaCertCmd())
 	teeCmd.AddCommand(uploadReportCmd())
@@ -59,4 +66,17 @@ func createClientWithConfig() (*sdk.ChainClient, error) {
 	}
 
 	return chainClient, nil
+}
+
+func createMultiSignAdmins(adminKeyFilePaths string, adminCrtFilePaths string) ([]string, []string, error){
+	adminKeys := strings.Split(adminKeyFilePaths, ",")
+	adminCrts := strings.Split(adminCrtFilePaths, ",")
+	if len(adminKeys) == 0 || len(adminCrts) == 0 {
+		return nil, nil, errors.New("no admin given for sign payload.")
+	}
+	if len(adminKeys) != len(adminCrts) {
+		return nil, nil, fmt.Errorf("admin keys num(%v) is not equals certs num(%v)", len(adminKeys), len(adminCrts))
+	}
+
+	return adminKeys, adminCrts, nil
 }
--- tools/cmc/tee/upload_report.go
+++ tools/cmc/tee/upload_report.go
@@ -8,6 +8,9 @@ SPDX-License-Identifier: Apache-2.0
 package tee
 
 import (
+	"chainmaker.org/chainmaker-go/tools/cmc/util"
+	"chainmaker.org/chainmaker/pb-go/common"
+	sdk "chainmaker.org/chainmaker/sdk-go"
 	"encoding/hex"
 	"fmt"
 	"io/ioutil"
@@ -34,11 +37,18 @@ func uploadReportCmd() *cobra.Command {
 	uploadReportCmd.Flags().AddFlagSet(flags)
 	uploadReportCmd.MarkFlagRequired("report")
 	uploadReportCmd.MarkFlagRequired("sdk-conf-path")
+	uploadReportCmd.MarkFlagRequired("admin-key-file-paths")
+	uploadReportCmd.MarkFlagRequired("admin-crt-file-paths")
 
 	return uploadReportCmd
 }
 
 func cliUploadReport() error {
+	adminKeys, adminCrts, err := createMultiSignAdmins(adminKeyFilePaths, adminCrtFilePaths)
+	if err != nil {
+		return err
+	}
+
 	file, err := os.Open(reportFile)
 	if err != nil {
 		return fmt.Errorf("open file '%s' error: %v", reportFile, err)
@@ -62,9 +72,27 @@ func cliUploadReport() error {
 		return fmt.Errorf("check new blockchains failed, %s", err.Error())
 	}
 
-	_, err = client.SaveEnclaveReport("global_enclave_id", reportData, "", true, 3)
+	payload, err := client.CreateSaveEnclaveReportPayload("global_enclave_id", reportData, "")
 	if err != nil {
-		return fmt.Errorf("save ca cert failed, %s", err.Error())
+		return fmt.Errorf("save enclave report failed, %s", err.Error())
+	}
+
+	endorsementEntrys := make([]*common.EndorsementEntry, len(adminKeys))
+	for i := range adminKeys {
+		e, err := sdk.SignPayloadWithPath(adminKeys[i], adminCrts[i], payload)
+		if err != nil {
+			return err
+		}
+		endorsementEntrys[i] = e
+	}
+
+	resp, err := client.SendContractManageRequest(payload, endorsementEntrys, 5, false)
+	if err != nil {
+		return err
+	}
+	err = util.CheckProposalRequestResp(resp, false)
+	if err != nil {
+		return err
 	}
 
 	fmt.Println("command execute successfully.")
