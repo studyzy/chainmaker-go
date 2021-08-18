5b946a2bb5c8a75140d45f11da33214c532f0c7d
Yuefeng Zhao <maxonzhao@gmail.com>
2021-08-18 19:39:22 +0800

    fix: recover missing script

---
+++ scripts/gomod_update.sh
@@ -0,0 +1,132 @@
+#
+# Copyright (C) BABEC. All rights reserved.
+# Copyright (C) THL A29 Limited, a Tencent company. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+set -x
+BRANCH=v2.0.2_dev
+
+cd ../module/accesscontrol
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../blockchain
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../conf/chainconf
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../localconf
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../../consensus
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../core
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../logger
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../net
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../rpcserver
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+#go test ./...
+cd ../snapshot
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../store
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../subscriber
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../sync
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../txpool
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../utils
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd ../vm
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go test ./...
+cd gasm
+go mod tidy
+cd ../evm
+go mod tidy
+cd ../wasi
+go mod tidy
+cd ../wasmer
+go mod tidy
+cd ../wxvm
+go mod tidy
+cd ../../../test
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+go build ./...
+cd ../tools/cmc
+go get chainmaker.org/chainmaker/protocol@${BRANCH}
+go get chainmaker.org/chainmaker/sdk-go@${BRANCH}
+go get chainmaker.org/chainmaker/pb-go@${BRANCH}
+go get chainmaker.org/chainmaker/common@${BRANCH}
+go mod tidy
+#go test ./...
+go build .
+cd ../../main
+go mod tidy
+go build .
