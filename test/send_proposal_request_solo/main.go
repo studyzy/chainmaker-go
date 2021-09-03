71ffb143b8fec3861f5952bd1deb220ef8f4af6a
taifu yuan <515789026@qq.com>
2021-09-03 11:57:41 +0800

    test: wasm contract binary update

--- test/send_proposal_request_solo/main.go
+++ test/send_proposal_request_solo/main.go
@@ -69,8 +69,8 @@ var caPaths = []string{certPathPrefix + "/crypto-config/wx-org1.chainmaker.org/c
 func main() {
 	common.SetCertPathPrefix(certPathPrefix)
 
-	//initWasmerTest()
-	//runTest()
+	initWasmerTest()
+	runTest()
 
 	initGasmTest()
 	runTest()
@@ -176,7 +176,9 @@ func runTest() {
 	// 18) 冻结、解冻、吊销用户合约功能测试
 	testFreezeOrUnfreezeOrRevokeFlow(sk3, client)
 
-	fmt.Println("    【runTest】 pass", "txId", txId)
+	fmt.Println("    【runTest】 all pass")
+	fmt.Println("    【runTest】 all pass")
+	fmt.Println("    【runTest】 all pass")
 }
 func initWasmerTest() {
 	WasmPath = "../wasm/rust-func-verify-2.0.0.wasm"
