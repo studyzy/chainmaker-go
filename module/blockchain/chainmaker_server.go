cd5baf00d5608eca299afe6821c71431a14b7550
taifu yuan <515789026@qq.com>
2021-10-25 17:24:46 +0800

    fix: stop all chain

--- module/blockchain/chainmaker_server.go
+++ module/blockchain/chainmaker_server.go
@@ -261,7 +261,7 @@ func (server *ChainMakerServer) Stop() {
 			defer wg.Done()
 			chain.Stop()
 		}(chain)
-		return false
+		return true
 	})
 	wg.Wait()
 	log.Info("ChainMaker server is stopped!")
