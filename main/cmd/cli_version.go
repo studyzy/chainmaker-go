78e6368314f6f292e317c205290e01eec6874f39
jasonruan <rzexin@qq.com>
2021-11-02 15:54:56 +0800

    move global.CurrentVersion to blockchain module

--- main/cmd/cli_version.go
+++ main/cmd/cli_version.go
@@ -7,9 +7,9 @@ SPDX-License-Identifier: Apache-2.0
 package cmd
 
 import (
+	"chainmaker.org/chainmaker-go/blockchain"
 	"fmt"
 
-	"chainmaker.org/chainmaker/localconf/v2"
 	"github.com/common-nighthawk/go-figure"
 	"github.com/spf13/cobra"
 )
@@ -31,16 +31,16 @@ func logo() string {
 	s := fig.String()
 	fragment := "================================================================================="
 	//versionInfo := "::ChainMaker::  version(" + protocol.DefaultBlockVersion + ")"
-	versionInfo := fmt.Sprintf("ChainMaker Version: %s\n", localconf.CurrentVersion)
+	versionInfo := fmt.Sprintf("ChainMaker Version: %s\n", blockchain.CurrentVersion)
 
-	if localconf.BuildDateTime != "" {
-		versionInfo += fmt.Sprintf("Build Time:%9s%s\n", " ", localconf.BuildDateTime)
+	if blockchain.BuildDateTime != "" {
+		versionInfo += fmt.Sprintf("Build Time:%9s%s\n", " ", blockchain.BuildDateTime)
 	}
 
-	if localconf.GitBranch != "" {
-		versionInfo += fmt.Sprintf("Git Commit:%9s%s", " ", localconf.GitBranch)
-		if localconf.GitCommit != "" {
-			versionInfo += fmt.Sprintf("(%s)", localconf.GitCommit)
+	if blockchain.GitBranch != "" {
+		versionInfo += fmt.Sprintf("Git Commit:%9s%s", " ", blockchain.GitBranch)
+		if blockchain.GitCommit != "" {
+			versionInfo += fmt.Sprintf("(%s)", blockchain.GitCommit)
 		}
 	}
 	return fmt.Sprintf("\n%s\n%s%s\n%s\n", fragment, s, fragment, versionInfo)
