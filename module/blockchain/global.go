3c164a44493586f793fe9cdf2e8f310800bc9a89
DevinZeng <studyzy@qq.com>
2021-11-02 14:05:52 +0800

    move global.CurrentVersion to blockchain module

---
+++ module/blockchain/global.go
@@ -0,0 +1,19 @@
+/*
+Copyright (C) BABEC. All rights reserved.
+Copyright (C) THL A29 Limited, a Tencent company. All rights reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package blockchain
+
+var (
+	//CurrentVersion current version
+	CurrentVersion = "v2.1.0"
+	// CurrentCommit current git commit hash
+	GitCommit = ""
+	// CurrentBranch current git branch
+	GitBranch = ""
+	// BuildDateTime compile datetime
+	BuildDateTime = ""
+)
